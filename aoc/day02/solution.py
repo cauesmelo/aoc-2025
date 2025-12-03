e04818c6d7b45716df47ca28238f51752065bfb7
cauesmelo <cauesmelo@gmail.com>
2025-12-03 20:27:31 -0300

    d2p1

--- aoc/day02/solution.py
+++ aoc/day02/solution.py
@@ -1,14 +1,43 @@
-"""Day 02: """
+"""Day 02:"""
 
-from aoc.utils import read_lines
+from aoc.utils import read_split_by_comma
 
 
 DAY = 2
 
 
+def is_invalid_id(id: str) -> bool:
+    if len(id) % 2 != 0:
+        return False
+
+    half = len(id) // 2
+    ptr1 = 0
+    ptr2 = half
+
+    while ptr1 < half:
+        if id[ptr1] != id[ptr2]:
+            return False
+
+        ptr1 += 1
+        ptr2 += 1
+
+    return True
+
+
 def part1(lines: list[str]) -> int:
     """Solve part 1."""
-    return 0
+
+    invalid_ids = 0
+
+    for line in lines:
+        id1, id2 = line.split("-")
+
+        for i in range(int(id1), int(id2) + 1):
+            if is_invalid_id(str(i)):
+                print("invalid -> ", i)
+                invalid_ids += i
+
+    return invalid_ids
 
 
 def part2(lines: list[str]) -> int:
@@ -17,12 +46,14 @@ def part2(lines: list[str]) -> int:
 
 
 def main():
-    lines = read_lines(DAY)
-    
+    # lines = read_split_by_comma(DAY, example=True)
+    lines = read_split_by_comma(DAY)
+
+    print(lines)
+
     print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    # print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
     main()
-
--- aoc/utils.py
+++ aoc/utils.py
@@ -3,23 +3,28 @@
 from pathlib import Path
 
 
-def read_input(day: int, filename: str = "input.txt") -> str:
+def read_input(day: int, example=False) -> str:
     """Read the input file for a given day."""
+    filename = "example.txt" if example else "input.txt"
     day_folder = Path(__file__).parent / f"day{day:02d}"
     return (day_folder / filename).read_text().strip()
 
 
-def read_lines(day: int, filename: str = "input.txt") -> list[str]:
+def read_lines(day: int, example=False) -> list[str]:
     """Read the input file as a list of lines."""
-    return read_input(day, filename).splitlines()
+    return read_input(day, example).splitlines()
 
 
-def read_ints(day: int, filename: str = "input.txt") -> list[int]:
+def read_ints(day: int, example=False) -> list[int]:
     """Read the input file as a list of integers."""
-    return [int(line) for line in read_lines(day, filename)]
+    return [int(line) for line in read_lines(day, example)]
 
 
-def read_grid(day: int, filename: str = "input.txt") -> list[list[str]]:
+def read_grid(day: int, example=False) -> list[list[str]]:
     """Read the input file as a 2D grid of characters."""
-    return [list(line) for line in read_lines(day, filename)]
+    return [list(line) for line in read_lines(day, example)]
 
+
+def read_split_by_comma(day: int, example=False) -> list[str]:
+    """Read the input file as a list of strings (lines representing the grid)."""
+    return read_input(day, example).split(",")
