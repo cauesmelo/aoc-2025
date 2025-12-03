268404944b901b3432baa0524fe64eabed466ee3
cauesmelo <cauesmelo@gmail.com>
2025-12-02 23:14:51 -0300

    wip d1p2

--- aoc/day01/solution.py
+++ aoc/day01/solution.py
@@ -1,5 +1,6 @@
 """Day 01:"""
 
+import math
 from aoc.utils import read_lines
 
 
@@ -13,8 +14,8 @@ def part1(lines: list[str]) -> int:
     password = 0
 
     for line in lines:
-        print("The dial is", dial_pos, end="")
-        print("\t|| rotated", line, end="")
+        # print("The dial is", dial_pos, end="")
+        # print("\t|| rotated", line, end="")
         value = int(line[1:])
 
         if line.startswith("L"):
@@ -23,7 +24,7 @@ def part1(lines: list[str]) -> int:
         else:
             dial_pos = (dial_pos + value) % 100
 
-        print("\t|| to point at", dial_pos)
+        # print("\t|| to point at", dial_pos)
 
         if dial_pos == 0:
             password += 1
@@ -33,11 +34,68 @@ def part1(lines: list[str]) -> int:
 
 def part2(lines: list[str]) -> int:
     """Solve part 2."""
-    return 0
+
+    dial_pos = 50
+    password = 0
+
+    for line in lines:
+        print("The dial is", dial_pos, end="")
+        print("\t|| rotated", line, end="")
+        value = int(line[1:])
+
+        clicked = False
+
+        if line.startswith("L"):
+            diff = dial_pos - value
+
+            if diff < 0:
+                clicks = math.ceil(-diff / 100)
+
+                if clicks > 0:
+                    clicked = True
+
+                password += clicks
+                print("\t|| clicks", clicks, end="")
+            else:
+                print("\t|| no clicks", end="")
+
+            dial_pos = diff % 100
+
+            print("\t|| diff", diff, end="")
+
+        else:
+            diff = dial_pos + value
+
+            if diff > 100:
+                clicks = abs(math.ceil(-diff / 100))
+
+                if clicks > 0:
+                    clicked = True
+
+                password += clicks
+                print("\t|| clicks", clicks, end="")
+            else:
+                print("\t|| no clicks", end="")
+
+            dial_pos = diff % 100
+            print("\t|| diff", diff, end="")
+
+        print("\t|| to point at", dial_pos, end="")
+
+        if dial_pos == 0 and clicked is False:
+            print("\t|| add to password")
+            password += 1
+        else:
+            print()
+
+        clicked = False
+
+    return password
 
 
 def main():
-    lines = read_lines(DAY)
+    # lines = read_lines(DAY)
+    lines = read_lines(DAY, filename="example.txt")
 
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
