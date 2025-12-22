197d83a1f5b0c7cd3c9e26f6bedbaaa5219d73d2
cauesmelo <cauesmelo@gmail.com>
2025-12-22 10:05:19 -0300

    d3p2

--- aoc/day03/solution.py
+++ aoc/day03/solution.py
@@ -6,12 +6,12 @@ from aoc.utils import read_lines
 DAY = 3
 
 
-def get_bigger_number_in_list(line: list[str], first_pick: bool):
+def get_bigger_number_in_list(line: list[str], skip: int):
     bigger = -1
     bigger_idx = 0
 
     for idx, ch in enumerate(line):
-        if first_pick and idx == len(line) - 1:
+        if idx == len(line) - skip:
             continue
 
         n = int(ch)
@@ -27,8 +27,8 @@ def part1(lines: list[str]) -> int:
     sum = 0
 
     for line in lines:
-        na, na_idx = get_bigger_number_in_list(line, True)
-        nb, nb_idx = get_bigger_number_in_list(line[na_idx + 1 :], False)
+        na, na_idx = get_bigger_number_in_list(line, 1)
+        nb, nb_idx = get_bigger_number_in_list(line[na_idx + 1 :], 0)
 
         sum += int(str(na) + str(nb))
 
@@ -37,14 +37,30 @@ def part1(lines: list[str]) -> int:
 
 def part2(lines: list[str]) -> int:
     """Solve part 2."""
-    return 0
+    sum = 0
+
+    for line in lines:
+        seq = ""
+        start_idx = 0
+        end_idx = len(line) - 11
+
+        for _ in range(11, -1, -1):
+            sliced_line = line[start_idx:end_idx]
+            n, idx = get_bigger_number_in_list(sliced_line, skip=0)
+            end_idx += 1
+            start_idx += idx + 1
+            seq += str(n)
+
+        sum += int(seq)
+
+    return sum
 
 
 def main():
     lines = read_lines(DAY, example=False)
 
     print(f"Part 1: {part1(lines)}")
-    # print(f"Part 2: {part2(lines)}")
+    print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
