a4fac341448daff49283c41ebbc4368deef34a4a
cauesmelo <cauesmelo@gmail.com>
2025-12-25 14:29:01 -0300

    d5p2

--- aoc/day05/solution.py
+++ aoc/day05/solution.py
@@ -25,8 +25,8 @@ def part1(lines: list[str]) -> int:
     fresh_count = 0
 
     for sku in skus:
-        for range in fresh_range:
-            if sku >= range[0] and sku <= range[1]:
+        for r in fresh_range:
+            if sku >= r[0] and sku <= r[1]:
                 fresh_count += 1
                 break
 
@@ -35,14 +35,38 @@ def part1(lines: list[str]) -> int:
 
 def part2(lines: list[str]) -> int:
     """Solve part 2."""
-    return 0
+    fresh_range: list[tuple[int, int]] = []
+
+    for line in lines:
+        if line == "":
+            break
+        else:
+            fresh_range.append(tuple(map(int, line.split("-"))))
+
+    fresh_range.sort()
+
+    merged = [fresh_range[0]]
+
+    for start, end in fresh_range[1:]:
+        last_start, last_end = merged[-1]
+
+        if start <= last_end + 1:
+            merged[-1] = (last_start, max(last_end, end))
+        else:
+            merged.append((start, end))
+
+    count = 0
+    for start, end in merged:
+        count += end - start + 1
+
+    return count
 
 
 def main():
     lines = read_lines(DAY, example=False)
 
     print(f"Part 1: {part1(lines)}")
-    # print(f"Part 2: {part2(lines)}")
+    print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
