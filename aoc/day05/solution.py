fb510c09ea33214f673d38a2d4efc24f25bfedf6
cauesmelo <cauesmelo@gmail.com>
2025-12-25 14:01:02 -0300

    d5p1

--- aoc/day05/solution.py
+++ aoc/day05/solution.py
@@ -1,4 +1,4 @@
-"""Day 05: """
+"""Day 05:"""
 
 from aoc.utils import read_lines
 
@@ -8,7 +8,29 @@ DAY = 5
 
 def part1(lines: list[str]) -> int:
     """Solve part 1."""
-    return 0
+
+    fresh_range: list[tuple[int, int]] = []
+    skus: list[int] = []
+    scanning_ranges = True
+
+    for line in lines:
+        if scanning_ranges:
+            if line == "":
+                scanning_ranges = False
+            else:
+                fresh_range.append(tuple(map(int, line.split("-"))))
+        else:
+            skus.append(int(line))
+
+    fresh_count = 0
+
+    for sku in skus:
+        for range in fresh_range:
+            if sku >= range[0] and sku <= range[1]:
+                fresh_count += 1
+                break
+
+    return fresh_count
 
 
 def part2(lines: list[str]) -> int:
@@ -17,12 +39,11 @@ def part2(lines: list[str]) -> int:
 
 
 def main():
-    lines = read_lines(DAY)
-    
+    lines = read_lines(DAY, example=False)
+
     print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    # print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
     main()
-
